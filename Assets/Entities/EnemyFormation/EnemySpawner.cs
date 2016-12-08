40cfa94ec265394b5b06323b63574a8dfa9ee2ce
Zachariasz <zah3@o2.pl>
2016-12-08 23:48:27 +0100

    First commit with LD

---
+++ Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+	
+	public GameObject enemyPrefab;
+	public float width = 10f;
+	public float hight = 3f;
+	
+	
+	// Use this for initialization
+	void Start () {
+		foreach (Transform child in transform){
+			GameObject enemy = Instantiate (enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
+			enemy.transform.parent = child;
+		}
+	}
+	public void OnDrowGizmos(){
+		Gizmos.DrawWireCube(transform.position, new Vector3(width, hight));	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Entities/Player/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 15.0f;
+	public float padding = 1.0f;
+	
+	float xmin =-5;
+	float xmax = 5;
+	
+	void Start () {
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0,0, distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1,0, distance));
+		xmin = leftmost.x + padding;
+		xmax = rightmost.x - padding;
+	}
+	
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}else if(Input.GetKey(KeyCode.RightArrow)){
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		
+		//restrict player to game space
+		float newX = Mathf.Clamp(transform.position.x,xmin,xmax);
+		transform.position = new Vector3(newX,transform.position.y, transform.position.z);
+	}
+}
