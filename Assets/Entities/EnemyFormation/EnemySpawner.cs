81c761b3f58a8c7e241fb6392993329900254bf2
Zachariasz <zah3@o2.pl>
2016-12-09 23:22:15 +0100

    Make enemyFormation flying

--- Assets/Entities/EnemyFormation/EnemySpawner.cs
+++ Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -8,10 +8,17 @@ public class EnemySpawner : MonoBehaviour {
 	public float hight = 6f;
 	public float speed = 5f;
 	private bool movingRight = true;
+	private float xMax;
+	private float xMin;
 	
 	
 	// Use this for initialization
 	void Start () {
+		float distanceToCamera = transform.position.z - Camera.main.transform.position.z;
+		Vector3 rightBoundary = Camera.main.ViewportToWorldPoint( new Vector3(1,0, distanceToCamera));
+		Vector3 leftBoundary = Camera.main.ViewportToWorldPoint( new Vector3(0,0, distanceToCamera));
+		xMax = rightBoundary.x;
+		xMin = leftBoundary.x;
 		foreach (Transform child in transform){
 			GameObject enemy = Instantiate (enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
 			enemy.transform.parent = child;
@@ -24,9 +31,14 @@ public class EnemySpawner : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(movingRight){
-			transform.position += new Vector3(speed*Time.deltaTime,0);
+			transform.position += Vector3.right * speed * Time.deltaTime;
 		}else {
-			transform.position += new Vector3(speed*Time.deltaTime,0);
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+		float rightEgdeOfFormation = transform.position.x +(0.5f*width);
+		float leftEdgeOfFormation = transform.position.x -(0.5f*width);
+		if(leftEdgeOfFormation < xMin || rightEgdeOfFormation > xMax){
+			movingRight = !movingRight;
 		}
 	}
 }
